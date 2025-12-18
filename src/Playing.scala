932e43b38910d71dc54143376fbd520feedaa01b
swiss-vsdev <gabriel.zeizer@gmail.com>
2025-12-18 16:53:33 +0100

    Remove KeyListener setup in Playing
    Eliminated the creation and assignment of a KeyListener to the display, as it was not working.

--- src/Playing.scala
+++ src/Playing.scala
@@ -6,9 +6,6 @@ object Playing extends App {
   val windowWidth : Int = 400
   val windowHeight : Int = 600
   val display : FunGraphics = new FunGraphics(windowWidth, windowHeight)
-  val kl : KeyListener = KeyListener
-
-  display.setKeyManager(kl)
 
   /*
   val game : Array[Array[Int]] = new Board()
